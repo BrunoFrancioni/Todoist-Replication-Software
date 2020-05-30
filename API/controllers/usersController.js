183cfa20e345cc2ddbf0d6fbc6876c065953f83f
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-05-30 19:58:07 -0300

    Little changes. Still not working the update user.

--- API/controllers/usersController.js
+++ API/controllers/usersController.js
@@ -3,7 +3,15 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 exports.LoginUser = (req, res) => {
-    Users.findOne({ where: { email: req.body.email } })
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        return res.status(400).json({
+            message: 'Email and password required.'
+        });
+    }
+
+    Users.findOne({ where: { email: email } })
         .then(user => {
             if(user === null) {
                 return res.status(401).json({
@@ -11,7 +19,7 @@ exports.LoginUser = (req, res) => {
                 })
             }
 
-            bcrypt.compare(req.body.password, user.password, (err, result) => {
+            bcrypt.compare(password, user.password, (err, result) => {
                 if(err) {
                     return res.status(401).json({
                         message: 'Auth failed.'
@@ -48,30 +56,37 @@ exports.LoginUser = (req, res) => {
 }
 
 exports.CreateUser = (req, res) => {
-    console.log(req.body);
-    Users.findOne({ where: { email: req.body.email } })
+    const { name, lastname, email, password } = req.body;
+
+    if(!name || !lastname || !email || !password) {
+        return res.status(400).json({
+            message: 'Name, lastname, email and password are required.'
+        });;
+    }
+
+    Users.findOne({ where: { email: email } })
         .then(user => {
             if(user !== null) {
                 return res.status(409).json({
                     message: 'Mail exists'
                 });
             } else {
-                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                bcrypt.hash(password, 10, (err, hash) => {
                     if (err) {
                         return res.status(500).json({
                             err
                         });
                     } else {
                         Users.create({
-                            name: (req.body.name).toUpperCase(),
-                            lastname: (req.body.lastname).toUpperCase(),
+                            name: name.toUpperCase(),
+                            lastname: lastname.toUpperCase(),
                             password: hash,
-                            email: req.body.email
+                            email: email
                         })
                             .then(result => {
                                 console.log(result);
                                 res.status(201).json({
-                                    message: 'User created'
+                                    message: 'User created.'
                                 });
                             })
                             .catch(error => {
@@ -92,10 +107,95 @@ exports.CreateUser = (req, res) => {
         });
 }
 
+exports.UpdateUser = (req, res) => {
+    const { name, lastname, password, email, oldPassword } = req.body;
+
+    let toUpdate = {};
+
+    if(name) toUpdate.name = name;
+    if(lastname) toUpdate.lastname = lastname;
+    if(email) toUpdate.email = email;
+
+    if(password) {
+        if(!oldPassword) {
+            return res.status(400).json({
+                message: 'Old password required.'
+            });
+        } else {
+            Users.findByPk(req.params.iduser)
+            .then(user => {
+                if(user === null) {
+                    return res.status(400).json({
+                        message: 'Bad user ID.'
+                    });
+                }
+
+                bcrypt.compare(oldPassword, user.password, (err, result) => {
+                    if(err) {
+                        return res.status(401).json({
+                            message: 'Wrong old password.'
+                        });
+                    }
+    
+                    if(result) {
+                        console.log(result);
+                    }
+                });
+            })
+            .catch(error => {
+                console.log(error);
+                return res.status(500).json({
+                    error
+                });
+            });
+        }
+    }
+
+    bcrypt.hash(password, 10, (err, hash) => {
+        if(err) {
+            return status(500).json({
+                err
+            });
+        }
+
+        if(hash) {
+            console.log(hash);
+            toUpdate.password = hash;
+        }
+    });
+
+    console.log(toUpdate);
+
+    if(Object.keys(toUpdate).length === 0) {
+        return res.status(400).json({
+            message: 'Nothing to change.'
+        });
+    } else {
+        Users.update(toUpdate, { where: { iduser: req.params.iduser } })
+            .then(result => {
+                console.log(result);
+                return res.status(200).json({
+                    message: 'User updated.'
+                });
+            })
+            .catch(error => {
+                return res.status(500).json({
+                    error
+                });
+            });
+    }
+}
+
 exports.DeleteUser = (req, res) => {
     console.log(req.params);
     Users.destroy({ where: { iduser: req.params.iduser }})
         .then(result => {
+            if(result === 0) {
+                return res.status(400).json({
+                    message: 'User ID not found.'
+                });
+            }
+
             res.status(200).json({
                 message: 'User deleted'
             });
