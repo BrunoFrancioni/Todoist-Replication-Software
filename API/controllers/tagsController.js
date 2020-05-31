2e070b9c3080033c349a3ab216cbdab69a962235
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-05-31 20:44:17 -0300

    Tags controller finished and method to delete all the instances of a tag to delete in Tasks Tagged table ready.

--- API/controllers/tagsController.js
+++ API/controllers/tagsController.js
@@ -3,7 +3,7 @@ const Users = require('../models/Users');
 
 const tasksTaggedController = require('../controllers/tasksTaggedController');
 
-exports.CreateTag = (req, res) => {
+exports.CreateTag = async (req, res) => {
     const { iduser, tagname } = req.body;
 
     if(!iduser || !tagname) {
@@ -12,62 +12,62 @@ exports.CreateTag = (req, res) => {
         });
     }
 
-    Users.findByPk(iduser)
-        .then(user => {
-            if(user === null) {
-                return res.status(400).json({
-                    message: 'User ID not found.'
-                });
-            } else {
-                Tags.create({
-                    iduser: user.iduser,
-                    tagname: tagname
-                })
-                .then(result => {
-                    console.log(result);
-                    res.status(200).json({
-                        message: 'Tag created.'
-                    })
-                })
-                .catch(error => {
-                    console.log(error);
-                    return res.status(500).json({
-                        error
-                    });
-                });
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            return res.status(500).json({
-                error
+    try {
+        const user = await Users.findByPk(iduser);
+
+        if(user === null) {
+            return res.status(400).json({
+                message: 'User ID not found.'
             });
+        }
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            error
         });
+    }
+
+    try {
+        const result = Tags.create({
+            iduser: iduser,
+            tagname: tagname
+        });
+
+        console.log(result);
+        res.status(200).json({
+            message: 'Tag created.'
+        });
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            error
+        });
+    }
 }
 
-exports.DeleteTag = (req, res) => {
+exports.DeleteTag = async (req, res) => {
     const result = tasksTaggedController.DeleteTaskTaggedFromTag(req.params.idtag);
 
     if(result === false) {
         return res.status(500);
     }
 
-    Tags.destroy({ where: { idtag: res.params.idtag } })
-        .then(result => {
-            if(result === 0) {
-                return res.status(400).json({
-                    message: 'Tag ID not valid.'
-                });
-            }
+    try {
+        const result = await Tags.destroy({ where: { idtag: res.params.idtag } });
 
-            res.status(200).json({
-                message: 'Tag deleted.'
+        if(result === 0) {
+            return res.status(400).json({
+                message: 'Tag ID not valid.'
             });
-        })
-        .catch(error => {
-            console.log(error);
+        }
+
+        res.status(200).json({
+            message: 'Tag deleted.'
+        });
+    } catch(error) {
+        console.log(error);
             res.status(500).json({
                 error
             });
-        });
+    }
 }
---
+++ API/controllers/tasksTaggedController.js
@@ -0,0 +1,99 @@
+const TasksTagged = require('../models/TasksTagged');
+const Tasks = require('../models/Tasks');
+const Tags = require('../models/Tags');
+
+exports.CreateTaskTagged = (req, res) => {
+    const { idtask, idtag } = req.body;
+
+    if(!idtask || !idtag) {
+        return res.status(400).json({
+            message: 'Task ID and Tag ID required.'
+        });
+    }
+
+    Tasks.findByPk(idtask)
+        .then(task => {
+            if(task === null) {
+                return res.status(400).json({
+                    message: 'Task ID not valid.'
+                });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            return res.status(500).json({
+                error
+            });
+        });
+
+    Tags.findByPk(idtag)
+        .then(tag => {
+            if(tag === null) {
+                return res.status(400).json({
+                    message: 'Tag ID not valid.'
+                });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            return res.status(500).json({
+                error
+            });
+        });
+
+    TasksTagged.create({
+        idtask, 
+        idtag
+    })
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Task tagged created.'
+            })
+        })
+        .catch(error => {
+            console.log(error);
+            return res.status(500).json({
+                error
+            });
+        });
+}
+
+exports.DeleteTaskTagged = (req, res) => {
+    TasksTagged.destroy({ where: { idtasktagged: idtasktagged } })
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: 'Task tagged deleted.'
+            })
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                error
+            });
+        });
+}
+
+exports.DeleteTaskTaggedFromTag = (idtag) => {
+    TasksTagged.destroy({ where: { idtag: idtag } })
+        .then(result => {
+            return true;
+        })
+        .catch(error => {
+            console.log(error);
+            return false;
+        });
+}
+
+exports.GetAllTagsOfATask = async (idtask) => {
+    try {
+        const result = await TasksTagged.destroy({ where: { idtask: idtask } });
+
+        console.log(result);
+        return true;
+    } catch(error) {
+        console.log(error);
+        return false;
+    }
+}
