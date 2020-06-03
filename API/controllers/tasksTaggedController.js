f6f66e29d4974970c5e6f7928f0657ac5a1ebdf0
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-02 22:07:53 -0300

    Tasks tagged controller finished with async await methods.

--- API/controllers/tasksTaggedController.js
+++ API/controllers/tasksTaggedController.js
@@ -2,7 +2,7 @@ const TasksTagged = require('../models/TasksTagged');
 const Tasks = require('../models/Tasks');
 const Tags = require('../models/Tags');
 
-exports.CreateTaskTagged = (req, res) => {
+exports.CreateTaskTagged = async (req, res) => {
     const { idtask, idtag } = req.body;
 
     if(!idtask || !idtag) {
@@ -11,79 +11,80 @@ exports.CreateTaskTagged = (req, res) => {
         });
     }
 
-    Tasks.findByPk(idtask)
-        .then(task => {
-            if(task === null) {
-                return res.status(400).json({
-                    message: 'Task ID not valid.'
-                });
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            return res.status(500).json({
-                error
+    try {
+        const task = await Tasks.findByPk(idtask);
+
+        if(task === null) {
+            return res.status(400).json({
+                message: 'Task ID not valid.'
             });
+        }
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            error
+        });
+    }
+
+    try {
+        const tag = await Tags.findByPk(idtag);
+
+        if(tag === null) {
+            return res.status(400).json({
+                message: 'Tag ID not valid.'
+            });
+        }
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            error
+        });
+    }
+
+    try {
+        const result = await TasksTagged.create({
+            idtask, 
+            idtag
         });
 
-    Tags.findByPk(idtag)
-        .then(tag => {
-            if(tag === null) {
-                return res.status(400).json({
-                    message: 'Tag ID not valid.'
-                });
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            return res.status(500).json({
-                error
-            });
+        console.log(result);
+        res.status(201).json({
+            message: 'Task tagged created.'
         });
-
-    TasksTagged.create({
-        idtask, 
-        idtag
-    })
-        .then(result => {
-            console.log(result);
-            res.status(201).json({
-                message: 'Task tagged created.'
-            })
-        })
-        .catch(error => {
-            console.log(error);
-            return res.status(500).json({
-                error
-            });
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            error
         });
+    }
 }
 
-exports.DeleteTaskTagged = (req, res) => {
-    TasksTagged.destroy({ where: { idtasktagged: idtasktagged } })
-        .then(result => {
-            console.log(result);
-            res.status(200).json({
-                message: 'Task tagged deleted.'
-            })
+exports.DeleteTaskTagged = async (req, res) => {
+    try {
+        const result = await TasksTagged.destroy({ where: { idtasktagged: req.params.idtasktagged } });
+
+        console.log(result);
+        res.status(200).json({
+            message: 'Task tagged deleted.'
         })
-        .catch(error => {
-            console.log(error);
-            res.status(500).json({
-                error
-            });
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            error
         });
+    }
 }
 
-exports.DeleteTaskTaggedFromTag = (idtag) => {
-    TasksTagged.destroy({ where: { idtag: idtag } })
-        .then(result => {
-            return true;
-        })
-        .catch(error => {
-            console.log(error);
-            return false;
-        });
+exports.DeleteTaskTaggedFromTag = async (idtag) => {
+    try {
+        const result = await TasksTagged.destroy({ where: { idtag: idtag } });
+
+        console.log(result);
+        return true;
+    } catch(error) {
+        console.log(error);
+        return false;
+    }
 }
 
 exports.DeleteAllTagsOfATask = async (idtask) => {
