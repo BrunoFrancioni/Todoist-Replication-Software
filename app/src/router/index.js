deeeaf18f5b3d4e7e7acee9ecf378cae5559fc53
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-25 09:32:10 -0300

    Firsts routes configured.

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Today',
+    component: () => import('../views/Today.vue')
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/inbox',
+    name: 'Inbox',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Inbox.vue')
+  },
+  {
+    path: '/upcoming',
+    name: 'Upcoming',
+    component: () => import('../views/Upcoming.vue')
   }
 ]
 
