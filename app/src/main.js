ad3730e8dd8eba44d2dffd7eeb204aa2909de5da
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-25 14:05:55 -0300

    BootstrapVue imported tothe project and working.

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,15 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
