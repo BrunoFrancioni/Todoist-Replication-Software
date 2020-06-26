878d1fa41c4253e5a57e477694e4e535aef5f49e
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-26 10:06:37 -0300

    Fixing main.js

--- app/src/main.js
+++ app/src/main.js
@@ -5,11 +5,8 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false
-
-Vue.use(BootstrapVue)
 
 new Vue({
   router,
