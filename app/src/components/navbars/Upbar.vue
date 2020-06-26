f48b2423d5e3d6ccb207e955381d093b597ea34d
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-26 13:31:01 -0300

    Upbar structure finished.

--- app/src/components/navbars/Upbar.vue
+++ app/src/components/navbars/Upbar.vue
@@ -1,31 +1,28 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <div class="bg-danger mb-4">
+        <nav class="nav bg-danger container">
+            <div class="form-inline">
+                <a class="navbar-brand" href="#"><i class="fas fa-bars text-white"></i></a>
+                <a href="#" class="nav-link"><i class="fas fa-home text-white"></i></a>
+                <form class="form-inline my-2 my-lg-0 input-group-sm">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Find">
+                    <button class="btn"><i class="fas fa-search text-white"></i></button>
+                </form>
+            </div>
 
-        <div class="collapse navbar-collapse" id="navbarColor02">
-            <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
-            </li>
+            <ul class="nav form-inline ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-plus text-white"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-chart-line text-white"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-cog text-white"></i></a>
+                </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
-    </nav>
+        </nav>
+    </div>
 </template>
 
 <script>
