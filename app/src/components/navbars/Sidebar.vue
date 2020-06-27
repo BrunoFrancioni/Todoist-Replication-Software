0078ef3e47dbf40b7dd68f8f524763d79c054bea
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-27 11:59:32 -0300

    Still working on the Sidebar.

--- app/src/components/navbars/Sidebar.vue
+++ app/src/components/navbars/Sidebar.vue
@@ -1,24 +1,39 @@
 <template>
   <div class="row mr-5 bg-sidebar">
-    <ul class="nav nav-pills flex-column">
-      <li class="nav-item">
-        <router-link to="/inbox" class="nav-link text-dark">Inbox</router-link>
+    <ul class="nav nav-pills flex-column pl-3 pt-2">
+      <li class="nav-item form-inline" @click="setActiveInbox()">
+        <i class="fas fa-inbox"></i>
+        <router-link 
+          to="/inbox" 
+          class="nav-link text-dark"
+          :class="{'font-weight-bold': inboxActive}"
+        >Inbox</router-link>
       </li>
-      <li class="nav-item">
-        <router-link to="/" class="nav-link text-dark font-weight-bold">Today</router-link>
+      <li class="nav-item form-inline" @click="setActiveToday()">
+        <i class="fas fa-calendar-day"></i>
+        <router-link 
+          to="/" 
+          class="nav-link text-dark"
+          :class="{'font-weight-bold': todayActive}"
+        >Today</router-link>
       </li>
-      <li class="nav-item">
-        <router-link to="/upcoming" class="nav-link text-dark">Upcoming</router-link>
+      <li class="nav-item form-inline mb-2" @click="setActiveUpcoming()">
+        <i class="far fa-calendar-alt"></i>
+        <router-link 
+          to="/upcoming" 
+          class="nav-link text-dark"
+          :class="{'font-weight-bold': upcomingActive}"
+        >Upcoming</router-link>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Separated link</a>
-        </div>
+
+      <li class="nav-item form-inline mb-2">
+        <i class="fas fa-sort-amount-down"></i>
+        <span class="nav-link text-dark font-weight-bold">Projects</span>
+      </li>
+
+      <li class="nav-item form-inline">
+        <i class="fas fa-sort-amount-down"></i>
+        <span class="nav-link text-dark font-weight-bold">Labels</span>
       </li>
     </ul>
   </div>
@@ -29,15 +44,29 @@ export default {
   name: 'Sidebar',
   data: () => {
     return {
-      active: ''
+      inboxActive: false,
+      todayActive: false,
+      upcomingActive: false
     }
   },
-  created: () => {
-    this.setActive('today');
+  created() {
+    this.setActiveToday();
   },
   methods: {
-    setActive: (active) => {
-      this.active = active;
+    setActiveInbox() {
+      this.inboxActive = true;
+      this.todayActive = false;
+      this.upcomingActive = false;
+    },
+    setActiveToday() {
+      this.inboxActive = false;
+      this.todayActive = true;
+      this.upcomingActive = false;
+    },
+    setActiveUpcoming() {
+      this.inboxActive = false;
+      this.todayActive = false;
+      this.upcomingActive = true;
     }
   }
 }
