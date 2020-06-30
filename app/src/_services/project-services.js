fe638041ce5a4a1dc0f2f52a6806acede25fb760
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-30 13:34:48 -0300

    Defining the project-services methods.

--- app/src/_services/project-services.js
+++ app/src/_services/project-services.js
@@ -1,6 +1,17 @@
 import http from './http-common';
 
 class projectServices {
+    CreateProject = async (data) => {
+        try {
+            const project = await http.post('/projects', data);
+
+            return project;
+        } catch(error) {
+            console.log(error);
+            return [];
+        }
+    }
+
     GetProjects = async (iduser) => {
         try {
             const projects = await http.get(`/projects/${iduser}`);
@@ -8,7 +19,40 @@ class projectServices {
             return projects;
         } catch(error) {
             console.log(error);
-            return [error];
+            return [];
+        }
+    }
+
+    GetProjectsArchived = async (iduser) => {
+        try {
+            const projects = await http.get(`/projects/${iduser}?archived=true`);
+
+            return projects;
+        } catch(error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    EditProject = async (idproject, data) => {
+        try {
+            const result = await http.put(`/projects/${idproject}`, data);
+
+            return result;
+        } catch(error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    DeleteProject = async (idproject) => {
+        try {
+            const result = await http.delete(`/projects/${idproject}`);
+
+            return result;
+        } catch(error) {
+            console.log(error);
+            return [];
         }
     }
 }
