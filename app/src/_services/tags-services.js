060c44404e54de5f113d2e5a17633e6e41ef04d7
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-07-01 15:24:20 -0300

    Developing Tags and Tags Tagged services.

--- app/src/_services/tags-services.js
+++ app/src/_services/tags-services.js
@@ -1,7 +1,49 @@
 import http from './http-common';
 
 class tagsServices {
+    CreateTag = async (data) => {
+        try {
+            const result = await http.post('/tags', data);
 
+            return result;
+        } catch(error) {
+            console.log(error);
+            return error;
+        }
+    }
+
+    GetTags = async (iduser) => {
+        try {
+            const tags = await http.get(`/tags/${iduser}`);
+
+            return tags;
+        } catch(error) {
+            console.log(error);
+            return error;
+        }
+    }
+
+    EditTag = async (idtag, data) => {
+        try {
+            const result = await http.put(`/tags/${idtag}`, data);
+
+            return result;
+        } catch(error) {
+            console.log(error);
+            return error;
+        }
+    }
+
+    DeleteTag = async (idtag) => {
+        try {
+            const result = await http.delete(`/tags/${idtag}`);
+
+            return result;
+        } catch(error) {
+            console.log(error);
+            return error;
+        }
+    }
 }
 
 export default new tagsServices();
--- app/src/_services/tasks-tagged-services.js
+++ app/src/_services/tasks-tagged-services.js
@@ -1,7 +1,27 @@
 import http from './http-common';
 
 class tasksTaggedServices {
+    CreateTaskTagged = async (data) => {
+        try {
+            const result = await http.post("/taskstagged", data);
 
+            return result;
+        } catch(error) {
+            console.log(error);
+            return error;
+        }
+    }
+
+    DeleteTaskTagged = async (idtasktagged) => {
+        try {
+            const result = await http.delete(`/taskstagged/${idtasktagged}`);
+
+            return result;
+        } catch(error) {
+            console.log(error);
+            return error;
+        }
+    }
 }
 
 export default new tasksTaggedServices();
