96682dc47ae50f45c4fa859a30cec220f1efe246
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-07-01 14:28:40 -0300

    Creating tags services and tasks tagged services.

---
+++ app/src/_services/tags-services.js
@@ -0,0 +1,7 @@
+import http from './http-common';
+
+class tagsServices {
+
+}
+
+export default new tagsServices();
---
+++ app/src/_services/tasks-tagged-services.js
@@ -0,0 +1,7 @@
+import http from './http-common';
+
+class tasksTaggedServices {
+
+}
+
+export default new tasksTaggedServices();
