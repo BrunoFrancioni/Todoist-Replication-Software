c63e6bd59b6f5776c22bbf13ca78b85cb33e40c1
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-07-02 13:55:04 -0300

    Developing the methods of the tasks services.

--- app/src/_services/tasks-services.js
+++ app/src/_services/tasks-services.js
@@ -1,7 +1,60 @@
 import http from './http-common';
 
 class tasksServices {
-    
+    CreateTask = async (data) => {
+        try {
+            const result = await http.post("/tasks", data);
+
+            return result;
+        } catch(error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    GetUserTasks = async (iduser, options) => {
+        try {
+            const result = await http.get(`/tasks/users/${iduser}?deleted=${options.deleted}&project=${options.deleted}`);
+
+            return result;
+        } catch(error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    GetProjectTasks = async (idproject, deleted) => {
+        try {
+            const result = await http.get(`/tasks/projects/${idproject}?deleted=${deleted}`);
+
+            return result;
+        } catch(error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    EditTask = async (idtask, data) => {
+        try {
+            const result = await http.put(`/tasks/${idtask}`, data);
+
+            return result;
+        } catch(error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    DeleteTask = async (idtask) => {
+        try {
+            const result = await http.delete(`/tasks/${idtask}`);
+
+            return result;
+        }catch(error) {
+            console.log(error);
+            return [];
+        }
+    }
 }
 
 export default new tasksServices();
