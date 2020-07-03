debb639487cc1db4d2d2d27073ee609b9708863f
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-07-03 13:56:30 -0300

    Fixing styles of Today and adding the tasks component.

--- app/src/views/Today.vue
+++ app/src/views/Today.vue
@@ -1,13 +1,43 @@
 <template>
-  <div class="ml-5">
+  <b-col class="" cols="12" md="8">
     <h1>Today</h1>
     <hr />
-  </div>
+    <tasks v-bind:tasks="tasks" />
+  </b-col>
 </template>
 
 <script>
+import tasksServices from '../_services/tasks-services'
+import Tasks from '../components/TasksComponents/Tasks'
+
 export default {
-    name: 'Today'
+  name: 'Today',
+  components: {
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.getTasks();
+  },
+  methods: {
+    async getTasks() {
+      const iduser = this.$parent.userInfo.iduser;
+      const options = {
+        deleted: false,
+        project: true
+      }
+
+      const result = await tasksServices.GetUserTasks(iduser, options);
+      console.log(result);
+      if(result.status === 200) {
+        this.tasks = result.data.tasks;
+      }
+    }
+  }
 }
 </script>
 
