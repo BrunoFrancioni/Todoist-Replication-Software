5e14c770fd157a5c8f3a1d54fbbce5c7e37a0dd8
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-07-04 20:25:12 -0300

    Project view creted.

---
+++ app/src/views/Project.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>{{ project.title }}</h1>
+    <hr />
+
+    <tasks v-if="tasks.length > 0" v-bind:tasks="tasks" />
+    <p v-else>No tasks for this project.</p>
+  </div>
+</template>
+
+<script>
+import Tasks from '../components/TasksComponents/Tasks'
+
+export default {
+    name: 'Project',
+    components: {
+        Tasks
+    },
+    props: ['project'],
+    data() {
+        return {
+            tasks: []
+        }
+    },
+    created() {
+        this.getTasks();
+    },
+    methods: {
+        async getTasks() {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
