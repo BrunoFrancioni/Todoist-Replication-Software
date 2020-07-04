4c50d675569a422316ec93a3da362953d4fab8f1
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-07-04 20:10:59 -0300

    Developing Inbox view.

--- app/src/views/Inbox.vue
+++ app/src/views/Inbox.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>
+  <b-col cols="12" md="8">
     <h1>Inbox</h1>
     <hr />
-  </div>
+
+    <tasks v-if="tasks.length > 0" v-bind:tasks="tasks" />
+    <p v-else>No tasks in INbox.</p>
+  </b-col>
 </template>
 
 <script>
+import tasksServices from '../_services/tasks-services'
+import Tasks from '../components/TasksComponents/Tasks'
+
 export default {
-    name: 'Inbox'
+  name: 'Inbox',
+  components: {
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.getTasks();
+  },
+  methods: {
+    async getTasks() {
+      const iduser = this.$parent.userInfo.iduser;
+
+      const result = await tasksServices.GetInboxUserTasks(iduser);
+
+      console.log(result);
+      if(result.status === 200) {
+        this.tasks = result.data.tasks;
+      }
+    }
+  }
 }
 </script>
 
