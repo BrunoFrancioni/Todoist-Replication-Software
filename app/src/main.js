8f4486dd04b5471c3aafbb493f8f5a7b440cb03b
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-07-07 19:46:09 -0300

    Added Sweet Alert to the project.

--- app/src/main.js
+++ app/src/main.js
@@ -5,9 +5,11 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import VueSweetalert2 from 'vue-sweetalert2'
 
 Vue.config.productionTip = false
+
+Vue.use(VueSweetalert2);
 
 new Vue({
   router,
