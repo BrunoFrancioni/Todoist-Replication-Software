6405f814df342bb991c36d235382c7725357215b
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-07-06 21:13:04 -0300

    Developing the modal to create a new task.

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <upbar v-on:setHomePage="setToday"/>
+    <upbar 
+      v-on:setHomePage="setToday"
+      @showModal="showModal"  
+    />
+
     <b-container>
       <div class="row">
         <sidebar ref="sidebar" />
@@ -9,6 +13,49 @@
         </transition>
       </div>
     </b-container>
+
+    <b-modal 
+      size="lg"
+      v-model="showCreateTaskModal" 
+      title="Create Task"
+      @hidden="resetModal"
+    >
+      <div>
+        <b-form >
+          <b-form-group
+            id="input-group-1"
+            label="Task title:"
+            label-for="title"
+          >
+            <b-form-input
+              id="title"
+              v-model="createTask.title"
+              type="text"
+              required
+              placeholder="Enter task title"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group 
+            id="input-group-2" 
+            label="Content:" 
+            label-for="content"
+          >
+            <b-form-input
+              id="content"
+              v-model="createTask.content"
+              required
+              placeholder="Enter the content"
+            ></b-form-input>
+          </b-form-group>
+
+          
+
+          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </b-form>
+      </div>
+    </b-modal>
   </div>
 </template>
 
@@ -24,12 +71,34 @@ export default {
   },
   data() {
     return {
-      userInfo: JSON.parse(atob(process.env.VUE_APP_DEV_TOKEN.split('.')[1]))
+      userInfo: JSON.parse(atob(process.env.VUE_APP_DEV_TOKEN.split('.')[1])),
+      showCreateTaskModal: false,
+      createTask: {
+        iduser: null,
+        idproject: null,
+        title: null,
+        content: null,
+        day: new Date(),
+        time: null
+      }
     }
   },
   methods: {
     setToday() {
       this.$refs.sidebar.setActiveToday();
+    },
+    showModal(){
+      this.showCreateTaskModal = true
+    },
+    resetModal() {
+      this.createTask = {
+        iduser: null,
+        idproject: null,
+        title: null,
+        content: null,
+        day: new Date(),
+        time: null
+      }
     }
   }
 }
