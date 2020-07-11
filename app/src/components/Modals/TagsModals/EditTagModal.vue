14bbf39179e5348b3e0377a91d397afcd4c5ba99
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-07-11 19:28:46 -0300

    Developed the modal to edit the tags and working fine.

---
+++ app/src/components/Modals/TagsModals/EditTagModal.vue
@@ -0,0 +1,86 @@
+<template>
+    <b-modal
+        v-model="showEditTagModal"
+        size="lg"
+        title="Edit Label"
+        @hidden="resetEditTagModal"
+    >
+        <div>
+            <b-form @submit="submitTagEdited" @reset="resetEditTagModal">
+                <b-form-group
+                    id="input-group-1"
+                    label="Label title:"
+                    label-for="title"
+                >
+                    <b-form-input
+                        id="title"
+                        v-model="editTag.tagname"
+                        type="text"
+                        required
+                        placeholder="Enter label name"
+                    ></b-form-input>
+                </b-form-group>
+
+                <b-form-group>
+                    <b-button type="submit" variant="primary">Save label</b-button>
+                </b-form-group>
+            
+            </b-form>
+        </div>
+
+        <template v-slot:modal-footer>
+            <b-button variant="" @click="closeModal">Close</b-button>
+        </template>
+    </b-modal>
+</template>
+
+<script>
+import tagsServices from '../../../_services/tags-services'
+
+export default {
+    name: 'EditTagModal',
+    props: ['showEditTagModal', 'editTag'],
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        resetEditTagModal() {
+            this.$parent.resetEditTagModal();
+        },
+        closeModal() {
+            this.$parent.closeEditTagModal();
+        },
+        async submitTagEdited(evt) {
+            evt.preventDefault();
+
+            const result = await tagsServices.EditTag(this.editTag.idtag, { tagname: this.editTag.tagname });
+
+            if(result.status !== 200) {
+                this.$parent.$parent.Toast.fire({
+                    icon: 'error',
+                    title: 'An error has occurred'
+                });
+
+                this.resetEditTagModal();
+            } else {
+                this.$parent.getLabels();
+                this.$parent.$parent.getTags();
+                this.$parent.resetTasks();
+
+                this.closeModal();
+
+                this.$parent.$parent.Toast.fire({
+                    icon: 'success',
+                    title: 'Label edited succesfully'
+                });
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
